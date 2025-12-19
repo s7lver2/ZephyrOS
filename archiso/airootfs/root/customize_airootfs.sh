26964b2250fb4885ce234029a3add37df7307fbd
s7lver2 <nickespro130@outlook.es>
2025-12-19 06:57:03 +0000

    FIX3: added archiso to the sudoers file

--- archiso/airootfs/root/customize_airootfs.sh
+++ archiso/airootfs/root/customize_airootfs.sh
@@ -3,6 +3,15 @@ systemctl set-default graphical.target
 systemctl enable sddm.service
 systemctl enable NetworkManager.service
 
+## Sudoers File Configuration
+mkdir -p /etc/sudoers.d
+
+cat <<EOF > /etc/sudoers.d/10-wheel-nopass
+# Allow arhiso user to use sudo with password
+%wheel ALL=(ALL) ALL
+EOF
+
+chmod 440 /etc/sudoers.d/10-wheel-nopasswd
 
 ## Create User
 useradd -m -u 1000 -G wheel,video,storage,optical -s /bin/bash archlive
@@ -12,12 +21,19 @@ cp -r /etc/skel/. /home/archlive
 chown -R archlive:archlive /home/archlive
 
 ## yay helper install
-sudo -u archlive <<'EOF'
+sudo -u archlive bash<<'EOF'
+set -e
+rm -rf yay
 cd /tmp
 git clone https://aur.archlinux.org/yay.git
 cd yay 
-makepkg -si --noconfirm
+makepkg -fsir --noconfirm --needed
+sudo mv yay /usr/local/bin/yay
+chmod 755 /usr/local/bin/yay
+cd /
 rm -rf /tmp/yay
+
+yay --version
 EOF
 
 ## calamares install
